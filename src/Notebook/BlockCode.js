1a98d62a29f90e9b07087f732d6a1b44482e3f84
hayateitan <eitan.hayat@gmail.com>
2021-10-15 13:22:41 +0300

    add drag and drop missing redorder

---
+++ src/Notebook/BlockCode.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFont } from "@fortawesome/free-solid-svg-icons";
+import { faGlasses } from "@fortawesome/free-solid-svg-icons";
+import { faChartLine } from "@fortawesome/free-solid-svg-icons";
+import Editeur from "./Editeur.js";
+import Explain from "./Predict.js";
+
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { v4 as uuidv4 } from "uuid";
+
+function BlockCode(props) {
+  const [editors, setEditors] = useState([]);
+
+  const addEditor = () => {
+    console.log("add");
+    let newEditors = [...editors, { id: uuidv4(), type: "editor" }];
+    setEditors(newEditors);
+  };
+
+  const addExplain = () => {
+    console.log("add explain");
+    let newEditors = [...editors, { id: uuidv4(), type: "explain" }];
+    setEditors(newEditors);
+  };
+
+  const createEditor = (e, i) => {
+    console.log(e);
+    return (
+      <Draggable key={e.id} draggableId={e.id} index={i}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {e.type === "editor" ? (
+              <Editeur id={e.id} key={e.id} />
+            ) : (
+              <Explain id={e.id} key={e.id} />
+            )}
+          </div>
+        )}
+      </Draggable>
+    );
+  };
+
+  const logEditor = (e) => {
+    if (e.type === "editor") {
+      console.log(document.getElementById("mydiv_" + e.id));
+    }
+  };
+  return (
+    <PerfectScrollbar>
+      <div id="iconPlus1Notebok" onClick={addEditor}>
+        <FontAwesomeIcon id="iconplus1" icon={faFont} />
+      </div>
+
+      <div id="iconPlus2Notebok">
+        <FontAwesomeIcon id="iconcode1" icon={faGlasses} onClick={addExplain} />
+      </div>
+
+      <div id="iconPlus3Notebok">
+        <FontAwesomeIcon id="iconplus3" icon={faChartLine} />
+      </div>
+      {/* 
+<div id="conatinerbuttonSauvegarde">
+    <Button>Backup</Button>
+</div> */}
+
+      <div id="divSetDisplaynone">
+        <div id="editeur-de-code">
+          <DragDropContext>
+            <Droppable droppableId="droppable">
+              {(provided, snapshot) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {editors?.map((e, i) => createEditor(e, i))}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+          {editors?.map((e) => logEditor(e))}
+        </div>
+      </div>
+    </PerfectScrollbar>
+  );
+}
+
+export default BlockCode;
--- src/Notebook/Notebook.js
+++ src/Notebook/Notebook.js
@@ -1,211 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import { useHistory } from "react-router-dom";
-import './style.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFont } from '@fortawesome/free-solid-svg-icons'
-import { faGlasses } from '@fortawesome/free-solid-svg-icons'
-import { faChartLine } from '@fortawesome/free-solid-svg-icons'
-import Editeur from './Editeur.js'
-import Explain from './Predict.js'
-import { v4 as uuidv4 } from 'uuid';
-import PerfectScrollbar from 'react-perfect-scrollbar'
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import { v4 as uuidv4 } from "uuid";
 
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import { useDispatch } from "react-redux";
-import { test } from '../actions/testaction'
+import { test } from "../actions/testaction";
+import BlockCode from "./BlockCode";
 // import { Button } from 'react-bootstrap';
 
-
 const Notebook = () => {
-    const dispath = useDispatch();
+  const dispath = useDispatch();
 
-    let history = useHistory();
+  const [zones, setZones] = useState([{ id: uuidv4() }, { id: uuidv4() }]);
+
+  /*let history = useHistory();
     let token = sessionStorage.getItem('jwt');
     if (token === null || token === undefined) {
         history.push("/login");
-    }
+    }*/
 
-    useEffect(() => {
-        dispath(test("eitan"))
-    },[])
+  /*useEffect(() => {
+    dispath(test("eitan"));
+  }, []);*/
 
-    // useEffect(() => {
-    //     timer = setTimeout(mafonction, 3000);
-    // }, [])
+  // useEffect(() => {
+  //     timer = setTimeout(mafonction, 3000);
+  // }, [])
 
-    // function mafonction() {
-    //     document.getElementById("editeur-de-code").style.visibility = "hidden"
-    // }
+  // function mafonction() {
+  //     document.getElementById("editeur-de-code").style.visibility = "hidden"
+  // }
 
-    // function onMouseOver() {
-    //     clearTimeout(timer)
-    //     document.getElementById("editeur-de-code").style.visibility = "visible"
-    //     timer = setTimeout(mafonction, 3000);
-    // }
+  // function onMouseOver() {
+  //     clearTimeout(timer)
+  //     document.getElementById("editeur-de-code").style.visibility = "visible"
+  //     timer = setTimeout(mafonction, 3000);
+  // }
 
-    // let timer
+  // let timer
 
-    const [editors, setEditors] = useState([])
+  return (
+    <DragDropContext>
+      <Droppable droppableId="droppable">
+        {(provided, snapshot) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {zones.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <BlockCode />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
 
-    const addEditor = () => {
-        console.log("add")
-        let newEditors = [...editors, { id: uuidv4(), type: "editor" }]
-        setEditors(newEditors)
-    }
-
-    const addExplain = () => {
-        console.log("add explain")
-        let newEditors = [...editors, { id: uuidv4(), type: "explain" }]
-        setEditors(newEditors)
-    }
-
-    const createEditor = (e) => {
-        console.log(e)
-        if (e.type === "editor") {
-            return <Editeur id={e.id} key={e.id} />
-        }
-        else {
-            return <Explain id={e.id} key={e.id} />
-        }
-    }
-
-    const logEditor = (e) => {
-
-        if (e.type === "editor") {
-            console.log(document.getElementById("mydiv_" + e.id))
-        }
-
-    }
-
-
-    return (
-        <PerfectScrollbar>
-
-
-
-            <div id="iconPlus1Notebok" onClick={addEditor}>
-                <FontAwesomeIcon id="iconplus1" icon={faFont} />
-
-            </div>
-
-
-            <div id="iconPlus2Notebok"  >
-
-                <FontAwesomeIcon id="iconcode1" icon={faGlasses} onClick={addExplain} />
-
-            </div>
-
-
-            <div id="iconPlus3Notebok"  >
-
-
-                <FontAwesomeIcon id="iconplus3" icon={faChartLine} />
-
-
-            </div>
-            {/* 
-            <div id="conatinerbuttonSauvegarde">
-                <Button>Backup</Button>
-            </div> */}
-
-
-
-       
-                        <div id="divSetDisplaynone" >
-                            <div id="editeur-de-code">
-
-
-                                {editors?.map(e => createEditor(e))}
-                                {editors?.map(e => logEditor(e))}
-
-                            </div>
-
-
-                        </div>
-              
-        </PerfectScrollbar>
-    )
-}
-
-export default Notebook
-
-
-
-// const finalSpaceNotebook = [
-//     {
-//         id: 'gary',
-//         name: 'Gary Goodspeed',
-//         thumb: '/images/gary.png'
-//     },
-//     {
-//         id: 'cato',
-//         name: 'Little Cato',
-//         thumb: '/images/cato.png'
-//     },
-//     {
-//         id: 'kvn',
-//         name: 'KVN',
-//         thumb: '/images/kvn.png'
-//     },
-//     {
-//         id: 'mooncake',
-//         name: 'Mooncake',
-//         thumb: '/images/mooncake.png'
-//     },
-//     {
-//         id: 'quinn',
-//         name: 'Quinn Ergon',
-//         thumb: '/images/quinn.png'
-//     }
-// ]
-
-// function App() {
-//     const [noteBook, updateNotebook] = useState(finalSpaceNotebook);
-
-//     function handleOnDragEnd(result) {
-//         if (!result.destination) return;
-
-//         const items = Array.from(noteBook);
-//         const [reorderedItem] = items.splice(result.source.index, 1);
-//         items.splice(result.destination.index, 0, reorderedItem);
-
-//         updateNotebook(items);
-//     }
-
-//     return (
-//         <div className="App">
-//             <header className="App-header">
-              
-//                 <DragDropContext onDragEnd={handleOnDragEnd}>
-//                     <Droppable droppableId="notebook">
-//                         {(provided) => (
-//                             <ul className="notebook" {...provided.droppableProps} ref={provided.innerRef}>
-//                                 {noteBook.map(({ id, name, thumb }, index) => {
-//                                     return (
-//                                         <Draggable key={id} draggableId={id} index={index}>
-//                                             {(provided) => (
-//                                                 <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-//                                                     <div className="characters-thumb">
-//                                                         <img src={thumb} alt={`${name} Thumb`} />
-//                                                     </div>
-//                                                     <p>
-//                                                         {name}
-//                                                     </p>
-//                                                 </li>
-//                                             )}
-//                                         </Draggable>
-//                                     );
-//                                 })}
-//                                 {provided.placeholder}
-//                             </ul>
-//                         )}
-//                     </Droppable>
-//                 </DragDropContext>
-//             </header>
-          
-//         </div>
-//     );
-// }
-
-// export default App;
+export default Notebook;
